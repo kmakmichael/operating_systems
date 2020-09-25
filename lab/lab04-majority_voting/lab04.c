f2e28fd7721e86c79472d0106bd84b376f110f6b
kmakmichael <m_kmak@u.pacific.edu>
2020-09-25 01:17:36 -0700

    lab04: step 1 complete

--- lab/lab04-majority_voting/lab04.c
+++ lab/lab04-majority_voting/lab04.c
@@ -2,5 +2,13 @@
 #include <stdint.h>
 
 int main() {
+	uint16_t num_voters;
+	uint16_t max_rounds;
+
+	printf("Number of voters? ");
+	scanf("%5hu", &num_voters);
+	printf("Rounds of voting? ");
+	scanf("%5hu", &max_rounds);
+	
 	return 0;
 }
