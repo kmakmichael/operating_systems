579e60cc8a9cee461fe752bcfc45333674193cbd
kmakmichael <m_kmak@u.pacific.edu>
2020-09-25 02:27:40 -0700

    lab04: pipe creation

--- lab/lab04-majority_voting/lab04.c
+++ lab/lab04-majority_voting/lab04.c
@@ -2,10 +2,12 @@
 #include <stdint.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <stdlib.h>
 
 int main() {
 	uint16_t num_voters = 0;
 	pid_t pid;
+	int **pipes;
 
 	// parameter input
 	while(1) {
@@ -17,6 +19,13 @@ int main() {
 	}
 
 	// pipe & child creation
-	// calloc(num_voters, sizeof(int [2]);
+	pipes = calloc(num_voters, sizeof(int *));
+	for (int i = 0; i < num_voters; i++)
+	{
+		pipes[i] = calloc(2, sizeof(int));
+		pipe(pipes[i]);
+	}
+
+	//execvp() or free(pipes)
 	return 0;
 }
