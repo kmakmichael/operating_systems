74e818bd2a548a3fbf0f7e6c3d53b68bd7d142bd
kmakmichael <m_kmak@u.pacific.edu>
2020-09-25 02:06:28 -0700

    lab04: adjust param input

--- lab/lab04-majority_voting/lab04.c
+++ lab/lab04-majority_voting/lab04.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <unistd.h>
+#include <sys/types.h>
 
 int main() {
-	uint16_t num_voters;
-	uint16_t max_rounds;
+	uint16_t num_voters = 0;
+	pid_t pid;
 
-	printf("Number of voters? ");
-	scanf("%5hu", &num_voters);
-	printf("Rounds of voting? ");
-	scanf("%5hu", &max_rounds);
-	
+	// parameter input
+	while(1) {
+		printf("Number of voters? ");
+		scanf("%5hu", &num_voters);
+		if(num_voters > 2)
+			break;
+		printf("There must be between 2 and 65536 voters\n");
+	}
+
+	// pipe & child creation
+	// calloc(num_voters, sizeof(int [2]);
 	return 0;
 }
