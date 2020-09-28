c533a80ad7117da1a8f0fc7dd463da55698e3849
kmakmichael <m_kmak@u.pacific.edu>
2020-09-27 22:57:52 -0700

    ic4.1: completed

--- in-class/ic4.1-pthreads/pthreads.c
+++ in-class/ic4.1-pthreads/pthreads.c
@@ -3,7 +3,9 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-uint64_t num_fibbs;
+//ugly globals for now, maybe fix later
+uint64_t *fibbs;
+
 void *fibby();
 
 
@@ -13,24 +15,43 @@ int main(int argc, char *argv[]) {
 	pthread_t tid;
 	pthread_attr_t attr;
 
-	// argument input checks
+	// handle params
 	if (argc != 2) {
 		fprintf(stderr, "usage: fibby.c <integer value>\n");
 		return -1;
 	}
-	if (atoi(argv[1]) < 1) {
-		fprintf(stderr, "%d must be 1 or greater", atoi(argv[1]));
+	uint64_t num_fibbs = atoi(argv[1]);
+	if (num_fibbs < 1) {
+		fprintf(stderr, "%lu must be greater than 1\n", num_fibbs);	
 		return -1;		
+	}
+	// 94 is the highest we can go with 64 bits
+	if (num_fibbs > 93) {
+		fprintf(stderr, "%lu must be less than 94\n", num_fibbs);
+		return -1;
 	}
 
 	// pthread prep and creation
+	if ( !(fibbs = calloc(num_fibbs, sizeof *fibbs)) ) {
+		fprintf(stderr, "could not alloc for %lu fibbonacci numbers!", num_fibbs);
+	}
 	pthread_attr_init(&attr);
 	pthread_create(&tid, &attr, fibby, argv[1]);
+
+	// wait and print
 	pthread_join(tid, NULL);
-	printf("hello\n");
+	printf("Generated %lu fibonacci numbers:\n", num_fibbs);
+	for(uint64_t i = 0; i < num_fibbs; i++) {
+		printf("%3lu:  %-10lu\n", i, fibbs[i]);
+	}
 	return 0;
 }
 
-void *fibby() {
-	;
+void *fibby(void *param) {
+	fibbs[0] = 0;
+	fibbs[1] = 1;
+	for (int i = 2; i < atoi(param); i++) {
+		fibbs[i] = fibbs[i-1] + fibbs[i-2];
+	}
+	pthread_exit(0);
 }
