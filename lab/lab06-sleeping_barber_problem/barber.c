814780ac66cead99390e2e8e114760ec1ca936f9
kmakmichael <m_kmak@u.pacific.edu>
2020-11-09 18:13:52 -0800

    lab06: skeleton code

--- lab/lab06-sleeping_barber_problem/barber.c
+++ lab/lab06-sleeping_barber_problem/barber.c
@@ -1,8 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <inttypes.h>
 #include <sys/types.h>
 #include <pthread.h>
+#include <unistd.h>
 
-int main() {
+int main(int argc, char *argv[]) {
+    // handle params
+	if (argc != 3) {
+		fprintf(stderr, "usage: barber.c <seats> <customers>\n");
+		return -1;
+	}
+	size_t seats = atoi(argv[1]);
+	if (seats < 1) {
+		fprintf(stderr, "Number of seats (%" PRIu64 ") must be greater than 1\n", seats);	
+		return -1;		
+	}
+	if (seats > 5) {
+		fprintf(stderr, "Number of seats (%" PRIu64 ") must be less than 6\n", seats);
+		return -1;
+	}
+    size_t customer_count = atoi(argv[2]);
+    if (customer_count < 1) {
+		fprintf(stderr, "Number of customers (%" PRIu64 ") must be greater than 1\n", seats);	
+		return -1;		
+	}
+	if (customer_count > 5) {
+		fprintf(stderr, "Number of customers (%" PRIu64 ") must be less than 256\n", seats);
+		return -1;
+	}
+    printf("Creating barber thread...\n");
+
+    printf("Barber thread created\n");
+
+
+    printf("Entering Main Loop\n");
+    for(size_t i = 0; i < customer_count; i++) {
+        sleep(1);
+        printf("Creating thread for customer %zu\n", i);
+    }
+
     return 0;
 }
