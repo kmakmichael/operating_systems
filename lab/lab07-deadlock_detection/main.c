8ab00dfece0ae6b05bb37f0beb43edbf2599c8f4
kmakmichael <m_kmak@u.pacific.edu>
2020-11-22 13:48:48 -0800

    lab07: begin

---
+++ lab/lab07-deadlock_detection/main.c
@@ -0,0 +1,15 @@
+/*
+    Lab 07: Deadlock Detection
+    COMP 173: Operating Systems (Fall 2020)
+    Michael Kmak
+
+    compilation: gcc main.c -o main
+    run: ./main <input_file>
+
+*/
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+
+    return 0;
+}
