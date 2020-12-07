6c6571ada7ea5de66712751f9f252a9d478fb678
kmakmichael <m_kmak@u.pacific.edu>
2020-12-06 21:47:36 -0800

    lab08: begin

---
+++ lab/lab08-paging_sim/paging_sim.c
@@ -0,0 +1,16 @@
+/*
+    Lab 08: Paging Simulation
+    COMP 173: Operating Systems (Fall 2020)
+    Michael Kmak
+
+    compilation: gcc paging_sim.c -o paging_sim
+    run: ./paging_sim
+*/
+#include <stdio.h>
+#include <inttypes.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int main() {
+    return 0;
+}
